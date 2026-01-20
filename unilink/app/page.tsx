1a10417255d7c5074a581ea85d91d1226d27872b
Namit Sood <namitsood@Namits-MacBook-Pro.local>
2026-01-20 20:31:05 +0530

    Initial Commit

---
+++ unilink/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-red-500">
+        Tailwind Test
+      </h1>
+    </div>
+  );
+}
